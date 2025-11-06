12632d3fff64a60deed904a13069f71bc07e3d35
Ani298 <animkrumova@abv.bg>
2025-11-06 19:04:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string Name(string name)
+        {
+            string input = "Hello, " + name;
+            return input;
+        }
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string result = Name(name);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
